cdce37791c18d19652ba3500b965b5ffbf186d56
Ken Beaton <ken.beaton@youi.tv>
2021-01-10 16:57:53 -0500

    Future proof by adding lots of Future calls.

--- lib/component/suggested.dart
+++ lib/component/suggested.dart
@@ -14,69 +14,117 @@ class SuggestedList extends StatefulWidget {
 }
 
 class _SuggestedList extends State<SuggestedList> {
-  // imgList is a list of image urls, set from the constructor.
-  List<Movie> movieList;
+  Future<List<Movie>> _movieList;
 
   @override
   void initState() {
     super.initState();
-    getSimilarMovieList(widget.movie);
+    getSimilarMovieList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(padding: const EdgeInsets.all(0.0), children: <Widget>[
-      /// Vertical parent widget container...
-      Container(
-          padding: EdgeInsets.all(0.0),
-          margin: EdgeInsets.symmetric(vertical: 0.0),
-          height: 300,
-          child: GridView.builder(
-              itemCount: movieList.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                childAspectRatio: 3.0 / 4.0,
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                final movie = movieList[index];
-                return Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Card(
-                        semanticContainer: true,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Expanded(
-                                  child: Container(
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          image: NetworkImage(movie.posterPath),
-                                          fit: BoxFit.fill,
-                                        ),
-                                      ),
-                                      child: GestureDetector(onTap: () {
-                                        Navigator.push<Widget>(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                ImageScreen(movie),
-                                          ),
-                                        );
-                                      })))
-                            ])));
-              }))
-    ]);
+    return Container(
+        padding: EdgeInsets.all(0.0),
+        margin: EdgeInsets.symmetric(vertical: 0.0),
+        child: FutureBuilder<List<Movie>>(
+            future:
+                _movieList, // a previously-obtained Future<List<Movie>> or null
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Movie>> snapshot) {
+              List<Widget> children;
+              List<Movie> movies = snapshot.data ?? [];
+              if (snapshot.hasData) {
+                children = <Widget>[
+                  Container(
+                      padding: EdgeInsets.all(0.0),
+                      margin: EdgeInsets.symmetric(vertical: 0.0),
+                      height: 240,
+                      child: GridView.builder(
+                          //physics: NeverScrollableScrollPhysics(),
+                          itemCount: movies.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            childAspectRatio: 3.0 / 4.0,
+                            crossAxisCount: 2,
+                          ),
+                          itemBuilder: (BuildContext context, int index) {
+                            final movie = movies[index];
+                            return Padding(
+                                padding: EdgeInsets.all(5),
+                                child: Card(
+                                    semanticContainer: true,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    clipBehavior: Clip.antiAlias,
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Expanded(
+                                              child: Container(
+                                                  decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                      image: NetworkImage(
+                                                          movie.posterPath),
+                                                      fit: BoxFit.fill,
+                                                    ),
+                                                  ),
+                                                  child: GestureDetector(
+                                                      onTap: () {
+                                                    Navigator.push<Widget>(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            ImageScreen(movie),
+                                                      ),
+                                                    );
+                                                  })))
+                                        ])));
+                          }))
+                ];
+              } else if (snapshot.hasError) {
+                children = <Widget>[
+                  Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                ];
+              } else {
+                children = <Widget>[
+                  SizedBox(
+                    child: CircularProgressIndicator(),
+                    width: 60,
+                    height: 60,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Loading...'),
+                  )
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            }));
   }
 
-  Future getSimilarMovieList(Movie aMovie) async {
-    List<Movie> movies = await MovieProvider.instance.getSimilarMovies(aMovie);
+  Future<List<Movie>> getSimilarMovieList() async {
+    Future<List<Movie>> list =
+        MovieProvider.instance.getSimilarMovies(widget.movie);
     // call setState here to set the actual list of items and rebuild the widget.
     setState(() {
-      movieList = movies;
+      _movieList = list;
     });
   }
 }
